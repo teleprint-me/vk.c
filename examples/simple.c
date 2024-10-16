62d23d77a824b6383170fbc87a43f28e70bb0a8d
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-10-16 17:21:39 -0400

    examples(simple): Add stub for drafting vulkan compute implementation

---
+++ examples/simple.c
@@ -0,0 +1,14 @@
+/**
+ * @file examples/simple.c
+ */
+
+#include <vulkan/vulkan.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// @note we only need a help text and the ability to pass a file path to the
+// shader being utilized for compute operations.
+int main(int argc, char* argv[]) {
+    return 0;
+}
