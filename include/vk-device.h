6264508489448f857dd0444b955f0e396f4f9670
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-10-17 00:32:19 -0400

    vk-device(feat): Add initial stub for the Device API

---
+++ include/vk-device.h
@@ -0,0 +1,14 @@
+/**
+ * Copyright © 2024 Austin Berrio
+ *
+ * @file include/vk-device.h
+ *
+ * @brief Vulkan Device API for managing Vulkan instances and devices.
+ */
+
+#ifndef VK_DEVICE
+#define VK_DEVICE
+
+#include <vulkan/vulkan.h>
+
+#endif // VK_DEVICE
