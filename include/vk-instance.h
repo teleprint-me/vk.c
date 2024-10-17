bf9bedf2b06bd4f5edf87ed64d03ebc45f05f83d
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-10-16 21:09:09 -0400

    vk-instance(feat): Add setters for instance info extensions and validation layers and clean up documentation

--- include/vk-instance.h
+++ include/vk-instance.h
@@ -1,5 +1,9 @@
 /**
+ * Copyright © 2024 Austin Berrio
+ *
  * @file include/vk-instance.h
+ * 
+ * @brief Create the VkInstance object with "sane" defaults.
  * 
  * @note Apply zero-initialization strategy to maintain a "sane" default implementation.
  */
@@ -9,14 +13,19 @@
 
 #include <vulkan/vulkan.h>
 
+// Create a struct VkApplicationInfo instance object
 struct VkApplicationInfo create_vk_application_info(char* pApplicationName, char* pEngineName);
 
+// Create a struct VkInstanceCreateInfo instance object
 struct VkInstanceCreateInfo create_vk_instance_info(struct VkApplicationInfo* pApplicationInfo);
 
-// dedicated function for adding extensions to an existing VkInstanceCreateInfo object instance
+// Set extensions for an existing VkInstanceCreateInfo object
+void set_instance_info_extensions(struct VkInstanceCreateInfo* pInstanceInfo, const char** extensions, uint32_t extensionCount);
 
-// dedicated function for adding validation layers to an existing VkInstanceInfo object instance
+// Set validation layers for an existing VkInstanceCreateInfo object
+void set_instance_info_validation_layers(struct VkInstanceCreateInfo* pInstanceInfo, const char** layers, uint32_t layerCount);
 
+// Create a struct VkInstance_T* (aka VkInstance) instance object
 struct VkInstance_T* create_vk_instance(struct VkInstanceCreateInfo* pInstanceInfo);
 
 #endif // VK_INSTANCE
--- src/vk-instance.c
+++ src/vk-instance.c
@@ -1,17 +1,16 @@
 /**
+ * Copyright © 2024 Austin Berrio
+ *
  * @file src/vk-instance.c
+ *
+ * @brief Create the VkInstance object with "sane" defaults.
  * 
  * @note Apply zero-initialization strategy to maintain a "sane" default implementation.
  */
 
 #include <vulkan/vulkan.h>
-
 #include <stdio.h>
 #include <stdlib.h>
-
-// ***
-// Create the Vulkan instance object
-// ***
 
 struct VkApplicationInfo create_vk_application_info(char* pApplicationName, char* pEngineName) {
     struct VkApplicationInfo applicationInfo = {};
@@ -28,27 +27,29 @@ struct VkInstanceCreateInfo create_vk_instance_info(struct VkApplicationInfo* pA
     struct VkInstanceCreateInfo instanceInfo = {};
     instanceInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
     instanceInfo.pApplicationInfo = pApplicationInfo;
-    // No extensions by default
     instanceInfo.enabledExtensionCount = 0;
     instanceInfo.ppEnabledExtensionNames = NULL;
-    // No validation layers by default
     instanceInfo.enabledLayerCount = 0;
     instanceInfo.ppEnabledLayerNames = NULL;
     return instanceInfo;
 }
 
-// dedicated function for adding extensions to an existing VkInstanceCreateInfo object instance
+void set_instance_info_extensions(struct VkInstanceCreateInfo* pInstanceInfo, const char** extensions, uint32_t extensionCount) {
+    pInstanceInfo->enabledExtensionCount = extensionCount;
+    pInstanceInfo->ppEnabledExtensionNames = extensions;
+}
 
-// dedicated function for adding validation layers to an existing VkInstanceInfo object instance
+void set_instance_info_validation_layers(struct VkInstanceCreateInfo* pInstanceInfo, const char** layers, uint32_t layerCount) {
+    pInstanceInfo->enabledLayerCount = layerCount;
+    pInstanceInfo->ppEnabledLayerNames = layers;
+}
 
 struct VkInstance_T* create_vk_instance(struct VkInstanceCreateInfo* pInstanceInfo) {
     struct VkInstance_T* pVkInstance;
-
     enum VkResult result = vkCreateInstance(pInstanceInfo, NULL, &pVkInstance);
     if (VK_SUCCESS != result) {
-        fprintf(stderr, "Failed to create Vulkan instance!\n");
+        fprintf(stderr, "Failed to create Vulkan instance! (Error code: %d)\n", result);
         exit(EXIT_FAILURE);
     }
-
     return pVkInstance;
 }
