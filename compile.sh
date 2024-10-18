dd06bcf22fab5d5530cc82f4cc60e277e3cd99c3
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-10-18 04:33:25 -0400

    compile(refactor): Allow passing build types (e.g., Debug, Release) as arguments to the script for more flexibility

--- compile.sh
+++ compile.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
-set -e  # Exit immediately if a command exits with a non-zero status.
+set -e
 
-rm -rf build
-cmake -B build -DCMAKE_BUILD_TYPE=Debug 
-cmake --build build --config Debug -j "$(nproc)"
+BUILD_PATH="build"
+BUILD_TYPE="${1:-Debug}"
+
+echo "Cleaning previous build..."
+rm -rf "${BUILD_PATH}"
+
+echo "Configuring the project with build type: $BUILD_TYPE"
+cmake -B "${BUILD_PATH}" -DCMAKE_BUILD_TYPE="${BUILD_TYPE}"
+
+echo "Building the project..."
+cmake --build "${BUILD_PATH}" --config "${BUILD_TYPE}" -j "$(nproc)"
+
+echo "Build completed successfully."
