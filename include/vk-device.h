4080d2714f745413583bc094064928e9027fd2f4
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-10-17 20:25:09 -0400

    vk-device(refactor): Restructure the API to maintain clarity and consistency

--- include/vk-device.h
+++ include/vk-device.h
@@ -29,19 +29,17 @@ typedef struct VulkanDevice {
     VkQueue queue; ///< Queue for the logical device
 } vulkan_device_t;
 
-// Functions for initializing and managing devices
+struct VkDeviceCreateInfo* create_device_info(void);
 
-struct VkDeviceCreateInfo create_device_info(void);
-
-struct VkDeviceQueueCreateInfo create_device_queue_info(void);
+struct VkDeviceQueueCreateInfo* create_device_queue_info(void);
 
 uint32_t get_physical_device_count(struct VkInstance_T* vkInstance);
 
-struct VkPhysicalDevice_T** create_physical_devices(
+struct VkPhysicalDevice_T** get_physical_devices(
     struct VkInstance_T* vkInstance, uint32_t deviceCount
 );
 
-void destroy_physical_devices(struct VkPhysicalDevice_T** physicalDevices);
+void free_physical_devices(struct VkPhysicalDevice_T** physicalDevices);
 
 struct VkPhysicalDeviceProperties get_physical_device_properties(
     struct VkPhysicalDevice_T* selectedPhysicalDevice,
@@ -53,49 +51,30 @@ struct VkPhysicalDevice_T* select_physical_device(
     uint32_t deviceCount
 );
 
-uint32_t get_physical_device_queue_family_count(
+uint32_t get_queue_family_property_count(
     struct VkPhysicalDevice_T* selectedPhysicalDevice
 );
 
-struct VkQueueFamilyProperties* create_queue_family_properties(
+struct VkQueueFamilyProperties* get_queue_family_properties(
     struct VkPhysicalDevice_T* selectedPhysicalDevice,
     uint32_t queueFamilyPropertyCount
 );
 
-void destroy_queue_family_properties(
+void free_queue_family_properties(
     struct VkQueueFamilyProperties* queueFamilyProperties
 );
 
-uint32_t get_physical_device_queue_family(
+uint32_t get_compute_queue_family_index(
     struct VkQueueFamilyProperties* queueFamilyProperties,
     uint32_t queueFamilyPropertyCount
 );
 
-/**
- * @brief Initializes the Vulkan device and selects the best available physical
- * device.
- *
- * @param vkInstance A pointer to a vulkan_instance_t structure.
- * @return A pointer to the created vulkan_device_t structure.
- */
+struct VkQueue_T* get_device_queue(
+    struct VkDevice_T* logicalDevice, uint32_t queueFamilyIndex
+);
+
 vulkan_device_t* create_vulkan_device(vulkan_instance_t* vkInstance);
 
-/**
- * @brief Cleans up and destroys the Vulkan device, including logical and
- * physical devices.
- *
- * @param vkDevice A pointer to the vulkan_device_t structure to destroy.
- */
 void destroy_vulkan_device(vulkan_device_t* vkDevice);
-
-/**
- * @brief Creates a queue for the logical device.
- *
- * @param vkDevice A pointer to the vulkan_device_t structure.
- * @param queueFamilyIndex Index of the queue family to create.
- */
-void create_device_queue(vulkan_device_t* vkDevice, uint32_t queueFamilyIndex);
-
-// Add other device-related functions as needed...
 
 #endif // VK_DEVICE
