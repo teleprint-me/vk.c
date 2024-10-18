c5ac69747fe6d921ae34d890b6383119a9d47387
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-10-18 03:31:39 -0400

    vk-instance(refactor): Allocate to stack to simplify implementation details and fix references accordingly

--- src/vk-instance.c
+++ src/vk-instance.c
@@ -14,68 +14,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct VkApplicationInfo* create_vk_application_info(char* pApplicationName, char* pEngineName) {
-    struct VkApplicationInfo* applicationInfo = (struct VkApplicationInfo*) malloc(
-        sizeof(struct VkApplicationInfo)
-    );
-
-    if (!applicationInfo) {
-        return NULL;
-    }
-
-    applicationInfo->sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-    applicationInfo->pApplicationName = pApplicationName;
-    applicationInfo->applicationVersion = VK_API_VERSION_1_0;
-    applicationInfo->pEngineName = pEngineName;
-    applicationInfo->engineVersion = VK_API_VERSION_1_0;
-    applicationInfo->apiVersion = VK_API_VERSION_1_2;
+struct VkApplicationInfo create_vk_application_info(
+    char* pApplicationName, char* pEngineName
+) {
+    struct VkApplicationInfo applicationInfo = {};
+    applicationInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    applicationInfo.pApplicationName = pApplicationName;
+    applicationInfo.applicationVersion = VK_API_VERSION_1_0;
+    applicationInfo.pEngineName = pEngineName;
+    applicationInfo.engineVersion = VK_API_VERSION_1_0;
+    applicationInfo.apiVersion = VK_API_VERSION_1_2;
     return applicationInfo;
 }
 
-struct VkInstanceCreateInfo* create_vk_instance_info(struct VkApplicationInfo* pApplicationInfo) {
-    struct VkInstanceCreateInfo* instanceInfo = (struct VkInstanceCreateInfo*) malloc(
-        sizeof(struct VkInstanceCreateInfo)
-    );
-
-    if (!instanceInfo) {
-        return NULL;
-    }
-
-    instanceInfo->sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-    instanceInfo->pApplicationInfo = pApplicationInfo;
-    instanceInfo->enabledExtensionCount = 0;
-    instanceInfo->ppEnabledExtensionNames = NULL;
-    instanceInfo->enabledLayerCount = 0;
-    instanceInfo->ppEnabledLayerNames = NULL;
+struct VkInstanceCreateInfo create_vk_instance_info(
+    struct VkApplicationInfo* pApplicationInfo
+) {
+    struct VkInstanceCreateInfo instanceInfo = {};
+    instanceInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    instanceInfo.pApplicationInfo = pApplicationInfo;
+    instanceInfo.enabledExtensionCount = 0;
+    instanceInfo.ppEnabledExtensionNames = NULL;
+    instanceInfo.enabledLayerCount = 0;
+    instanceInfo.ppEnabledLayerNames = NULL;
     return instanceInfo;
 }
 
-void set_instance_info_extensions(struct VkInstanceCreateInfo* pInstanceInfo, const char** extensions, uint32_t extensionCount) {
+void set_instance_info_extensions(
+    struct VkInstanceCreateInfo* pInstanceInfo,
+    const char* const* extensions,
+    uint32_t extensionCount
+) {
     pInstanceInfo->enabledExtensionCount = extensionCount;
     pInstanceInfo->ppEnabledExtensionNames = extensions;
 }
 
-void set_instance_info_validation_layers(struct VkInstanceCreateInfo* pInstanceInfo, const char** layers, uint32_t layerCount) {
+void set_instance_info_validation_layers(
+    struct VkInstanceCreateInfo* pInstanceInfo,
+    const char* const* layers,
+    uint32_t layerCount
+) {
     pInstanceInfo->enabledLayerCount = layerCount;
     pInstanceInfo->ppEnabledLayerNames = layers;
 }
 
-vulkan_instance_t* create_vulkan_instance(char* pApplicationName, char* pEngineName) {
-    vulkan_instance_t* vkInstance = (vulkan_instance_t*) malloc(
-        sizeof(vulkan_instance_t)
-    );
-
-    if (!vkInstance) {
-        return NULL;
-    }
-
-    vkInstance->applicationInfo = create_vk_application_info(
+vulkan_instance_t create_vulkan_instance(char* pApplicationName, char* pEngineName) {
+    vulkan_instance_t vkInstance = {};
+    vkInstance.applicationInfo = create_vk_application_info(
         pApplicationName, pEngineName
     );
-    vkInstance->instanceCreateInfo = create_vk_instance_info(vkInstance->applicationInfo);
+    vkInstance.instanceCreateInfo = create_vk_instance_info(&vkInstance.applicationInfo);
 
     VkResult result = vkCreateInstance(
-        vkInstance->instanceCreateInfo, NULL, &vkInstance->handle
+        &vkInstance.instanceCreateInfo, NULL, &vkInstance.handle
     );
     if (VK_SUCCESS != result) {
         fprintf(stderr, "Failed to create Vulkan instance! (Error code: %d)\n", result);
@@ -86,12 +77,7 @@ vulkan_instance_t* create_vulkan_instance(char* pApplicationName, char* pEngineN
 }
 
 void destroy_vulkan_instance(vulkan_instance_t* vkInstance) {
-    if (vkInstance) {
-        if (vkInstance->handle) {
-            vkDestroyInstance(vkInstance->handle, NULL); // Vulkan instance cleanup
-        }
-        free(vkInstance->applicationInfo); // Free application info
-        free(vkInstance->instanceCreateInfo); // Free instance info
-        free(vkInstance); // Finally, free the instance itself
+    if (vkInstance->handle) {
+        vkDestroyInstance(vkInstance->handle, NULL); // Vulkan instance cleanup
     }
 }
