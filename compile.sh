c4e5a36beb0498755e4294a9fb9ab794b8851470
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-10-18 04:31:52 -0400

    compile(feat): Exit immediately if a command exits with a non-zero status

--- compile.sh
+++ compile.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e  # Exit immediately if a command exits with a non-zero status.
+
 rm -rf build
 cmake -B build -DCMAKE_BUILD_TYPE=Debug 
 cmake --build build --config Debug -j "$(nproc)"
