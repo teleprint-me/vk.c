df113ace139de0bbaeb464ecc4c6849d13736a08
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-10-19 16:47:15 -0400

    vk(feat): Add implementation for manually setting up the vulkan device object

--- examples/vk.c
+++ examples/vk.c
@@ -6,6 +6,10 @@
  * @note The primary objective with this example program is to perform summation
  * using a discrete GPU with the Vulkan C API. The vk.c library is exposed to
  * showcase the underlying logic.
+ * 
+ * @todo Need to enable debugging as the project scope grows before it becomes an issue. It is
+ * currently omitted to intentionally keep things as simple as possible due to the nature of
+ * how complex the Vulkan C API is.
  */
 
 #include "vk-instance.h"
@@ -29,7 +33,8 @@ int main(void) {
     const char* applicationName = "ComputeApp";
     const char* engineName = "ComputeEngine";
 
-    // Allocate memory with zero-initialization
+    // Allocate memory to the custom vkInstance
+    // @note We should not reference this object as the vkInstance. This is confusing.
     vulkan_instance_t* vkInstance = calloc(1, sizeof(vulkan_instance_t));
     if (NULL == vkInstance) {
         fprintf(stderr, "vulkan_create_instance: Failed to allocate memory for VulkanInstance\n");
@@ -78,7 +83,59 @@ int main(void) {
 
     /**
      * Setup the Vulkan Device Object
+     * 
+     * @todo Add example comments for extensions and validation layers
      */
+    // Allocate memory to custom vkDevice
+    // @note We should not reference this object as the vkDevice. This is confusing.
+    vulkan_device_t* vkDevice = (vulkan_device_t*) malloc(sizeof(vulkan_device_t));
+
+    // Enumerate and select the physical device
+    vkDevice->physicalDeviceCount = vulkan_enumerate_physical_device_count(vkInstance);
+    VkPhysicalDevice* physicalDeviceList = vulkan_enumerate_physical_device_list(
+        vkInstance, vkDevice->physicalDeviceCount
+    );
+    vkDevice->physicalDevice = vulkan_select_physical_device(
+        physicalDeviceList, vkDevice->physicalDeviceCount
+    );
+    vulkan_free_device_list(physicalDeviceList);
+
+    // Get device properties
+    vkDevice->physicalDeviceProperties = vulkan_get_physical_device_properties(
+        vkDevice->physicalDevice
+    );
+
+    // Create the queue create info on the stack
+    uint32_t queueFamilyPropertyCount = vulkan_get_queue_family_property_count(vkDevice->physicalDevice);
+    VkQueueFamilyProperties queueFamilyProperties[16];
+    vulkan_get_queue_family_properties(vkDevice->physicalDevice, queueFamilyProperties);
+    
+    uint32_t computeQueueFamilyIndex = vulkan_get_compute_queue_family_index(queueFamilyProperties, queueFamilyPropertyCount);
+
+    // Create queue creation info
+    VkDeviceQueueCreateInfo queueCreateInfo = vulkan_create_device_queue_info(computeQueueFamilyIndex);
+
+    // Set up the device creation info
+    VkDeviceCreateInfo deviceCreateInfo = vulkan_create_device_info(&queueCreateInfo, 1);
+
+    // Create the logical device
+    VkResult result = vkCreateDevice(vkDevice->physicalDevice, &deviceCreateInfo, NULL, &vkDevice->logicalDevice);
+
+    if (VK_SUCCESS != result) {
+        fprintf(stderr, "Failed to create logical device! (Error code: %d)\n", result);
+        exit(EXIT_FAILURE);
+    }
+
+    // Retrieve the queue handle from the logical device
+    vkGetDeviceQueue(vkDevice->logicalDevice, computeQueueFamilyIndex, 0, &vkDevice->logicalQueue);
+
+    // Create the queue structure and assign the handle
+    vkDevice->queueFamilyIndex = computeQueueFamilyIndex;
+
+    /**
+     * Output status
+     */
+    printf("Silence is golden <3\n");
 
     /********************************
      * Clean up allocated resources *
@@ -87,6 +144,12 @@ int main(void) {
     /** 
      * Destroy the Vulkan Device Object
      */
+    if (vkDevice != NULL) {
+        if (vkDevice->logicalDevice != VK_NULL_HANDLE) {
+            vkDestroyDevice(vkDevice->logicalDevice, NULL);
+        }
+        free(vkDevice);
+    }
 
     /** 
      * Destroy the Vulkan Instance Object
