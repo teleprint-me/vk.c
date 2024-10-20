0caa4d9a04e5bc7c9e75beb5964c0d00856196ae
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-10-20 01:02:59 -0400

    examples(shader): Add CLI option support to mitigate shader file path handling

--- examples/shader.c
+++ examples/shader.c
@@ -14,11 +14,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <getopt.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+void print_usage(char* argv[]) {
+    fprintf(
+        stderr,
+        "Usage: "
+        "%s [--help] "
+        "path/to/file.comp\n",
+        argv[0]
+    );
+}
+
+// Define command line option structure
+static struct option long_options[]
+    = {{"help", 0, NULL, 'h'},
+       {0, 0, NULL, 0}};
+
 int main(int argc, char* argv[]) {
     const char* appName = "DeviceApp";
     const char* engineName = "DeviceEngine";
-    const char* shaderPath = "path/to/shader.spv";  // Path to your SPIR-V shader file
-    
+    char* shaderPath = NULL;  // Path to SPIR-V shader file
+
+    while (true) {
+        static int opt_index = 0;
+
+        int c = getopt_long(argc, argv, "h:s:n:m:", long_options, &opt_index);
+
+        if (c == -1) {
+            break;
+        }
+
+        switch (c) {
+            case 'h':
+                print_usage(argv);
+                return EXIT_SUCCESS;
+            case 's':
+                // Convert seed input
+                shaderPath = (char*) optarg;
+                if (NULL == shaderPath) {
+                    fprintf(stderr, "Failed to create GLSL shader path.\n");
+                    return EXIT_FAILURE;
+                }
+                printf("Setting position to: %d\n", shaderPath);
+                break;
+            default:
+                print_usage(argv);
+                return EXIT_FAILURE;
+        }
+    }
+
     // Create the instance object
     vulkan_instance_t* vkInstance = vulkan_create_instance(appName, engineName);
     if (NULL == vkInstance) {
