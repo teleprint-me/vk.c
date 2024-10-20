4d77e42a11edc1e11465442cc43d86dfa7cc34ab
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-10-19 21:49:34 -0400

    vk-shader(feat): Add a structure to encapsulate shader-related data, e.g. the shader module, file buffer, and size

--- include/vk-shader.h
+++ include/vk-shader.h
@@ -10,16 +10,25 @@
 #include <vulkan/vulkan.h>
 #include <stdio.h>
 
+/**
+ * @brief Structure representing a Vulkan shader and related data.
+ */
+typedef struct VulkanShader {
+    VkShaderModule module;  ///< Handle to the Vulkan shader module
+    char* buffer;           ///< Buffer containing the shader code
+    size_t bufferSize;      ///< Size of the shader code buffer
+} vulkan_shader_t;
+
 // @brief Creates and returns a VkShaderModuleCreateInfo structure with default values.
 VkShaderModuleCreateInfo vulkan_create_shader_module_info(void);
 
-// @brief Reads the shader module file and returns the FILE*.
+// @brief Reads the shader module file and returns the buffer.
 FILE* vulkan_read_shader_module(const char* filepath);
 
 // @brief Calculates the size of the shader module file.
 size_t vulkan_calculate_shader_module_size(FILE* file);
 
-// @brief Creates a buffer for the shader module based on the file size.
+// @brief Creates the shader module buffer based on the file size.
 char* vulkan_create_shader_module_buffer(FILE* file, size_t fileSize);
 
 // @brief Sets up the VkShaderModuleCreateInfo structure with the buffer and file size.
@@ -27,9 +36,10 @@ void vulkan_setup_shader_module_info(
     VkShaderModuleCreateInfo* shaderInfo, char* buffer, size_t fileSize
 );
 
-// @brief Creates the shader module using the device and shader module info.
-VkShaderModule vulkan_create_shader_module(
-    VkDevice device, VkShaderModuleCreateInfo* shaderInfo, const char* filepath
-);
+// @brief Initializes the vulkan_shader_t structure and creates the shader module.
+vulkan_shader_t* vulkan_create_shader(VkDevice device, const char* filepath);
+
+// @brief Destroys the shader module and frees related resources.
+void vulkan_destroy_shader(VkDevice device, vulkan_shader_t* shader);
 
 #endif // VK_SHADER_H
