82fba70fb694a4c063e03b7d32870f59475fc3ba
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-10-19 21:43:14 -0400

    vk-shader(refactor): Fix inclusion guards, add inline comments, and add some basic formatting to improve readability

--- include/vk-shader.h
+++ include/vk-shader.h
@@ -4,20 +4,32 @@
  * @file include/vk-shader.h
  */
 
-#ifndef VK_SHADER
-#define VK_SHADER
+#ifndef VK_SHADER_H
+#define VK_SHADER_H
 
 #include <vulkan/vulkan.h>
 #include <stdio.h>
 
-FILE* read_shader_module(const char* filepath);
-size_t calculate_shader_module_size(FILE* file);
-char* create_shader_module_buffer(FILE* file, size_t fileSize);
-void setup_shader_module_info(
+// @brief Creates and returns a VkShaderModuleCreateInfo structure with default values.
+VkShaderModuleCreateInfo vulkan_create_shader_module_info(void);
+
+// @brief Reads the shader module file and returns the FILE*.
+FILE* vulkan_read_shader_module(const char* filepath);
+
+// @brief Calculates the size of the shader module file.
+size_t vulkan_calculate_shader_module_size(FILE* file);
+
+// @brief Creates a buffer for the shader module based on the file size.
+char* vulkan_create_shader_module_buffer(FILE* file, size_t fileSize);
+
+// @brief Sets up the VkShaderModuleCreateInfo structure with the buffer and file size.
+void vulkan_setup_shader_module_info(
     VkShaderModuleCreateInfo* shaderInfo, char* buffer, size_t fileSize
 );
-VkShaderModule create_shader_module(
+
+// @brief Creates the shader module using the device and shader module info.
+VkShaderModule vulkan_create_shader_module(
     VkDevice device, VkShaderModuleCreateInfo* shaderInfo, const char* filepath
 );
 
-#endif // VK_SHADER
+#endif // VK_SHADER_H
